8a07f7bead9727fc0e63a81e9ba7322d47fb2a70
Kcity162 <kevin.torrington@gmail.com>
2025-06-18 21:04:13 +0100

    Added the basic functions of search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { Autocomplete, TextField, Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
-function App() {
+const users = [
+  { name: 'Alice Johnson', code: '123456', avatar: 'https://i.pravatar.cc/40?u=alice' },
+  { name: 'Bob Smith', code: '654321', avatar: 'https://i.pravatar.cc/40?u=bob' },
+  { name: 'Charlie Lee', code: '112233', avatar: 'https://i.pravatar.cc/40?u=charlie' },
+];
+
+export default function UserSearch() {
   return (
-    <div>
-      <h1>Hello, MUI!</h1>
-      <Button variant="contained" color="primary">
-        Click me
-      </Button>
-    </div>
+    <Autocomplete
+      options={users}
+      getOptionLabel={(option) => `${option.name} (${option.code})`}
+      renderOption={(props, option) => (
+        <ListItem {...props}>
+          <ListItemAvatar>
+            <Avatar src={option.avatar} />
+          </ListItemAvatar>
+          <ListItemText primary={option.name} secondary={`Code: ${option.code}`} />
+        </ListItem>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search for a user"
+          variant="outlined"
+          fullWidth
+          size="large"
+        />
+      )}
+      sx={{ width: 400 }}
+    />
   );
 }
-
-export default App;
