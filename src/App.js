051213e990d147c81198f5cdcc61fae6f8f796d0
Kcity162 <kevin.torrington@gmail.com>
2025-06-18 21:11:16 +0100

    Basics done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Autocomplete, TextField, Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { Autocomplete, TextField, Avatar, ListItem, ListItemAvatar, ListItemText, Box } from '@mui/material';
 
 const users = [
   { name: 'Alice Johnson', code: '123456', avatar: 'https://i.pravatar.cc/40?u=alice' },
@@ -9,27 +9,37 @@ const users = [
 
 export default function UserSearch() {
   return (
-    <Autocomplete
-      options={users}
-      getOptionLabel={(option) => `${option.name} (${option.code})`}
-      renderOption={(props, option) => (
-        <ListItem {...props}>
-          <ListItemAvatar>
-            <Avatar src={option.avatar} />
-          </ListItemAvatar>
-          <ListItemText primary={option.name} secondary={`Code: ${option.code}`} />
-        </ListItem>
-      )}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search for a user"
-          variant="outlined"
-          fullWidth
-          size="large"
-        />
-      )}
-      sx={{ width: 400 }}
-    />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        height: '100vh',
+        paddingTop: '30px',
+      }}
+    >
+      <Autocomplete
+        options={users}
+        getOptionLabel={(option) => `${option.name} (${option.code})`}
+        renderOption={(props, option) => (
+          <ListItem {...props}>
+            <ListItemAvatar>
+              <Avatar src={option.avatar} />
+            </ListItemAvatar>
+            <ListItemText primary={option.name} secondary={`Code: ${option.code}`} />
+          </ListItem>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search for a user"
+            variant="outlined"
+            fullWidth
+            size="large"
+          />
+        )}
+        sx={{ width: 400 }}
+      />
+    </Box>
   );
 }
